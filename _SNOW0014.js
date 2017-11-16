53a37340a0b5563bd471ff54680df42b452f7e70
Chaitanya <go4chaitu@gmail.com>
2017-11-16 18:48:47 +0530

    Update _SNOW0014.js

--- _SNOW0014.js
+++ _SNOW0014.js
@@ -11,7 +11,6 @@ module.exports = function(context){
 		
 		"BlockStatement:exit"(node) {
 			if(node && node.body && node.body.length  === 0) {
-                        console.log('aaaa -> ' + node.parent.parent.key.type)
                 if(!isOOTBMethod(node)) {
 					context.report(node, "Scripts should not have an empty blocks defined ");
                 }
